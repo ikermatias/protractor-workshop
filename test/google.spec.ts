4345ceeddf37c381213690079586f39e3facfef7
Joan Sebastián García Delgado <josegar0218@gmail.com>
2019-05-04 11:24:20 -0500

    add static code (#8)

--- test/google.spec.ts
+++ test/google.spec.ts
@@ -2,12 +2,12 @@ import { browser } from 'protractor';
 
 describe('Given a SDET learning protractor', () => {
   describe('when open Google Page', () => {
-   beforeEach(async () => {
+    beforeEach(async () => {
       await browser.get('http://www.google.com');
     });
 
     it('then should have a title', async () => {
-        expect(await browser.getTitle()).toEqual('Google');
+      expect(await browser.getTitle()).toEqual('Google');
     });
   });
 });
